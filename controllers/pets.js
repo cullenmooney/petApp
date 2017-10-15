e53b131936483f64fa8904b03533d8b49016d091
Cullen Mooney <cullenmooney@Cullens-MacBook.local>
2017-10-15 12:55:41 -0500

    update fuctionality now working

--- controllers/pets.js
+++ controllers/pets.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const router = express.Router();
-const pet = require('../models/pets');
+const pets = require('../models/pets');
 
 router.get('/', (req, res) => {
-	res.render('index', {})
+	res.render('index', {pet: pets})
 })
 
 router.get('/new', (req, res) => {
 	res.render('new', {})
 })
 
+router.get('/:index', (req, res) => {
+	res.render('show', {pet: pets[req.params.index]})
+})
+
+router.get('/:index/edit', (req, res) => {
+	res.render('edit', {pet: pets[req.params.index], index: req.params.index})
+	res.redirect('/pets');
+})
+
+router.put('/:index/edit', (req, res) => {
+	pets[req.params.index] = req.body
+	res.redirect('/pets')
+})
 module.exports = router;
---
+++ models/pets.js
@@ -0,0 +1,6 @@
+const pets = [{name: "spike", species: "dog", age: 5, img: "http://www.freepngimg.com/thumb/dog/5-dog-png-image-thumb.png"},
+				{name: "jerry", species: "mouse", age: 3, img: "https://upload.wikimedia.org/wikipedia/en/2/2f/Jerry_Mouse.png"},
+				{name: "tom", species: "cat", age: 6, img: "https://vignette.wikia.nocookie.net/tomandjerry/images/3/33/Tom_cat.png/revision/latest?cb=20111113021359"}
+				];
+
+module.exports = pets;
